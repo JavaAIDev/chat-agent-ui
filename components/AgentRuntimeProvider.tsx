eca3a73519ad61ebe61d382333452d0ed7ea5a63
Fu Cheng <alexcheng1982@gmail.com>
2025-03-29 23:35:13 +1300

    add ts ignore

--- components/AgentRuntimeProvider.tsx
+++ components/AgentRuntimeProvider.tsx
@@ -35,6 +35,7 @@ const AgentModelAdapter: ChatModelAdapter = {
         .pipeThrough(new EventSourceParserStream());
 
     let text = "";
+    // @ts-ignore
     for await (const content of eventStream) {
       let data = content.data || "";
       if (data.length > 2) {
